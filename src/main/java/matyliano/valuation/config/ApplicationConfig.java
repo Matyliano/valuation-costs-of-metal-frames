07a1be33814ba826db62feaeb305792ad10e5083
Matyliano <joanna.boszko@gmail.com>
2019-03-13 13:15:35 +0100

    + JpaConfig, AppConfig, WebMvcConfig & WebAppInitializer

---
+++ src/main/java/matyliano/valuation/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package matyliano.valuation.config;
+
+
+import matyliano.valuation.ValuationCostsOfMetalFramesApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:persistence.properties")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackageClasses = ValuationCostsOfMetalFramesApplication.class)
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
